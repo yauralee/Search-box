26efdfa215340ea420fa8a505e0bb2ac79d09e7d
liyu <LiYu_2506@163.com>
2015-11-18 18:10:10 +0800

    first commit

---
+++ search.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  function timeFormat(timeStamp){
+    var date = new Date(parseInt(timeStamp));
+    return "Created@"+date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate()+1)；
+  }
+  function appendBookmarks(data){
+    var searchResult = data.reduce(function(str,item){
+      str+="<li>"+data.title+"</li>";
+      str+="<li>"+timeFormat(data.created)+"</li>";
+      str+="<hr>";
+      return str;
+    },"");
+    $('ul').html(searchResult);
+  }
+  $.ajax({
+    url:"bookmarks.json",
+    type:"get",
+    dataType:"json",
+    success:function(data){
+      appendBookmarks(data);
+    }
+  })
+
+  function showResult(keyWords,bookmarks){
+    $("#displayArea").html("");
+    var reg = new RegExp(keywords,"ig");
+    var selectedData = bookmarks.filter(function(item){
+      return reg.test(item.title);
+    }).map(function(item){
+      return item.title.replace(reg,'<span class="hightlightStyle">$1</span>');
+    })
+    appendBookmarks(selectedData);
+  }
+
+  $("#keywordInput").bind("input propertychange",function(){
+     $.ajax({
+       url:"bookmarks.json",
+       type:"get",
+       dataType:"json",
+       success:function(data){
+         var inputWords = $(this).val();
+         showResult(inputWords,data);
+       }
+     })
+  });
+})
